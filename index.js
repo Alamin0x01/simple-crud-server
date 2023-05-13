be5249896d990a1a8eefb991fa4d23771d77afbb
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-05-13 11:20:41 +0600

    dotenv added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 9000;
 
@@ -8,9 +9,8 @@ const port = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://nalaminmmc5:Gx2zhCtEFDgOvop1@cluster0.37liajg.mongodb.net/?retryWrites=true&w=majority";
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.37liajg.mongodb.net/?retryWrites=true&w=majority`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
